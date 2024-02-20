76b065b1498e2f78f6602c130f3fed80235f5bcc
Martin Sernow (msg) <ckudo16@gmail.com>
2024-02-20 17:17:24 +0100

    Monster DB angepasst

--- assets/monster/monsterDB.gd
+++ assets/monster/monsterDB.gd
@@ -1,27 +1,55 @@
 class_name MonsterDB extends Node
 
-# [Name, LP, ATK, Speed, Magic, DEF, Image, flip, Lore]
-enum {Dragoblin, Widbear, Zephyrclaw, Phoenix, FlameheartBeast, AbyssalShadow, StormbringerWyrm, NightsongSpecter, EternalFrostWarden}
+# [LP, ATK, Speed, Magic, DEF, flip, Lore]
+enum {AbyssalShadow, BreezyWooler, CelestialGuardian, CluckstertheMystic, CrystalWyrm, DeathWitch, Dragoblin, EternalFrostWarden, FlameheartBeast, ForestFidget, LuminousBloom, MeadowMischief, MoonlightHopper, NightsongSpecter, Phoenix, StormbringerWyrm, StormscaleLeviathan, Thunderhoof, TimeWeaver, VoidStalker, WhisperingShade, Widbear, Zephyrclaw}
 
-const DATA = {
-	Dragoblin: 
-		["Dragoblin", 2, 2, 3, 1, 3, "dragoblin.png", true],
-	Widbear: 
-		["Widbear", 3, 4, 4, 1, 2, "widbear.png", true],
-	Zephyrclaw: 
-		["Zephyrclaw", 5, 3, 4, 3, 4, "zephyrclaw.png", false, "Es ist eine eindrucksvolle Mischung aus einem eisigen Drachen und einem schattenhaften Panther, umgeben von wirbelndem Schnee und Nebel."],
-	Phoenix: 
-		["Phoenix", 3, 1, 3, 3, 2, "phoenix.png", true],
-	FlameheartBeast: 
-		["Flameheart Beast", 5, 1, 3, 3, 2, "FlameheartBeast.png", false, "Es ist eine beeindruckende Kombination aus einem Löwen und einem Phönix, umgeben von vulkanischen Kräften, ein Symbol für die unkontrollierbare Natur des Feuers."],
-	AbyssalShadow: 
-		["Abyssal Shadow", 3, 1, 3, 3, 2, "AbyssalShadow.png", false, "Es ist eine mysteriöse Kreatur, die die Eleganz eines Panthers mit den geheimnisvollen Aspekten eines Tiefsee-Wesens kombiniert, umgeben von nebelverhangenen Abgründen."],
-	StormbringerWyrm: 
-		["Stormbringer Wyrm", 4, 1, 3, 3, 2, "StormbringerWyrm.png", false, "Es ist ein majestätisches Wesen, das die Kräfte von Sturm und Blitz kontrolliert, thront auf einer hohen Klippe, umgeben von wirbelnden Wolken und dem Brausen des Windes."],
-	NightsongSpecter: 
-		["Nightsong Specter", 2, 1, 3, 3, 2, "NightsongSpecter.png", true, "Es ist ein Wesen, das aus den Schatten selbst geboren wurde, ein Meister der Illusion und der Furcht, umhüllt von einem nebelartigen Dunst und der Dunkelheit der Nacht."],
-	EternalFrostWarden: 
-		["Eternal Frost Warden", 5, 4, 4, 1, 2, "EternalFrostWarden.png", false],
+const DATA: Dictionary = {
+	 AbyssalShadow:
+		 [2, 2, 2, 2, 2, false],
+	 BreezyWooler:
+		 [2, 2, 2, 2, 2, false],
+	 CelestialGuardian:
+		 [2, 2, 2, 2, 2, false],
+	 CluckstertheMystic:
+		 [2, 2, 2, 2, 2, false],
+	 CrystalWyrm:
+		 [2, 2, 2, 2, 2, false],
+	 DeathWitch:
+		 [2, 2, 2, 2, 2, false],
+	 Dragoblin:
+		 [2, 2, 2, 2, 2, true],
+	 EternalFrostWarden:
+		 [2, 2, 2, 2, 2, true],
+	 FlameheartBeast:
+		 [2, 2, 2, 2, 2, false],
+	 ForestFidget:
+		 [2, 2, 2, 2, 2, false],
+	 LuminousBloom:
+		 [2, 2, 2, 2, 2, false],
+	 MeadowMischief:
+		 [2, 2, 2, 2, 2, false],
+	 MoonlightHopper:
+		 [2, 2, 2, 2, 2, true],
+	 NightsongSpecter:
+		 [2, 2, 2, 2, 2, true],
+	 Phoenix:
+		 [2, 2, 2, 2, 2, true],
+	 StormbringerWyrm:
+		 [2, 2, 2, 2, 2, false],
+	 StormscaleLeviathan:
+		 [2, 2, 2, 2, 2, false],
+	 Thunderhoof:
+		 [2, 2, 2, 2, 2, false],
+	 TimeWeaver:
+		 [2, 2, 2, 2, 2, true],
+	 VoidStalker:
+		 [2, 2, 2, 2, 2, true],
+	 WhisperingShade:
+		 [2, 2, 2, 2, 2, true],
+	 Widbear:
+		 [2, 2, 2, 2, 2, true],
+	 Zephyrclaw:
+		 [2, 2, 2, 2, 2, false],
 }
 
 static func rand():
--- szenen/gegner.gd
+++ szenen/gegner.gd
@@ -1,7 +1,8 @@
 class_name Gegner extends VBoxContainer
 
 var monsterName = MonsterDB.rand()
-var monster = MonsterDB.DATA.get(monsterName)
+# Leben, ATK, Speed, Magic, DEF, Flip
+var monster: Array = [1, 1, 1, 1, 1, false]
 
 var hp_value := 1
 var previes_hp_value := 1
@@ -18,7 +19,7 @@ func _ready():
 	change_monster();
 
 func _process(delta):
-	hp_value = clampi(hp_value, 0, monster[1] * hp_scale)
+	hp_value = clampi(hp_value, 0, monster[0] * hp_scale)
 		
 	if hp_value != previes_hp_value:
 		if hp_value > previes_hp_value:
@@ -31,27 +32,25 @@ func _process(delta):
 		ko = true
 		koed.emit()
 	
-	if monster[1] <= 0 and dead == false:
+	if monster[0] <= 0 and dead == false:
 		dead = true
 		died.emit()
 	
 	# Render
-	%Avatar/VBoxContainer/Atk.value = monster[2]
-	%Avatar/VBoxContainer/Speed.value = monster[3]
-	%Avatar/VBoxContainer/Magie.value = monster[4]
-	%Avatar/VBoxContainer/Def.value = monster[5]
-	%LP.max_value = monster[1] * hp_scale
+	%Avatar/VBoxContainer/Atk.value = monster[1]
+	%Avatar/VBoxContainer/Speed.value = monster[2]
+	%Avatar/VBoxContainer/Magie.value = monster[3]
+	%Avatar/VBoxContainer/Def.value = monster[4]
+	%LP.max_value = monster[0] * hp_scale
 	%LP.value = hp_value
 
 func change_monster():
 	monster = MonsterDB.DATA.get(monsterName)
-	hp_value = monster[1] * hp_scale
+	hp_value = monster[0] * hp_scale
 	previes_hp_value = hp_value
-	$Avatar.texture = load(str("res://assets/monster/", monster[6]))
-	$Name.text = monster[0]
-	$Avatar.flip_h = monster[7]
-	
-
+	$Avatar.texture = load(str("res://assets/monster/", monsterName, ".png"))
+	$Name.text = monsterName.capitalize()
+	$Avatar.flip_h = monster[5]
 
 func _on_timer_timeout():
 	monsterName = MonsterDB.rand()
